903bc41fffb166891857f6f530443152e84470c3
klairt <klair.tulip.studente@itispaleocapa.it>
2026-02-10 08:57:15 +0100

    Add files via upload

---
+++ crud.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+
+using namespace std;
+
+void view(int &v[], &d)
+{
+	for(int i=0;i<d,i++)
+}
+
+void add(int v[],int &d)
+{
+	cout<<"Inserire il valore:";
+	cin>>x;
+	v[d]=x;
+	d++
+	
+}
+
+int main()
+{
+	int a[100];
+	int dim=1;
+	do
+	{
+		cout<<"1 - Inserimento\n";
+		cout<<"2 - Cancellazione\n";
+		cout<<"3 - Modifica\n";
+		cout<<"4 - Visualizzazione\n";
+		cout<<"Scegli opzione:";
+		
+		cin>>opzione;
+		
+		switch(opzione)
+		{
+			case 1: //Inserimento
+					
+					add(a,dim)
+					break;
+			case 2: 
+					break;
+			case 3: 
+					break;
+			case 4: 
+					break;
+		
+				
+		}
+		
+	}while
+
+}
